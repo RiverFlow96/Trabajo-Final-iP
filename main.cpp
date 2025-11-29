55d0750c58d65c0d4a0bb73a2e807384b96a29d5
RiverFlow <yordanyalmeda8@gmail.com>
2025-11-29 09:18:05 -0500

    Agregando array de defectos

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const int tiradas = 3;
+    const int cant_productos = 4;
+
+    int defectos[tiradas][cant_productos] = {
+        {1, 2, 3, 4}, 
+        {1, 2, 3, 4}, 
+        {1, 2, 3, 4}
+    };
+
+    return 0;
+}
