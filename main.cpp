554b48c93acb3f9914cb0d09100a5934b0a65e39
RiverFlow <yordanyalmeda8@gmail.com>
2025-11-29 09:21:46 -0500

    Agregando calculo del total

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+int total(int array){
+    int total = 0;
+
+    for (int tirada = 0; tirada < 3; tirada++){
+        for(int producto = 0; producto < 4; producto++){
+            total += producto;
+        }
+    }
+
+    return total;
+
+}
+
 int main(){
 
     const int tiradas = 3;
@@ -12,6 +25,8 @@ int main(){
         {1, 2, 3, 4}, 
         {1, 2, 3, 4}
     };
+
+    cout<<total(defectos[tiradas][cant_productos]);
 
     return 0;
 }
