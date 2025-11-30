2c57152c11de0bcd34b7873e6c5df4e5eba5c112
RiverFlow <yordanyalmeda8@gmail.com>
2025-11-30 09:21:56 -0500

    Funcionalidades de mayor y menor cantidad de defectos agregada

--- main.cpp
+++ main.cpp
@@ -6,20 +6,40 @@ using namespace std;
 struct Resultados
 {
     int total;
+    int mayor;
+    int menor;
 };
 
 
 //Funcion para calcular el total
 Resultados Funcionalidades(int tiradas, int cant_productos, vector<vector<int>>& array){
     int total = 0;
+    vector<int>total_por_tiradas(tiradas, 0);
+    int tirada_menor = 0;
+    int tirada_mayor = 0;
 
+    // calculo del total
     for (int tirada = 0; tirada < tiradas; tirada++){
         for(int producto = 0; producto < cant_productos; producto++){
             total += array[tirada][producto];
+            total_por_tiradas[tirada] += array[tirada][producto];
         }
     }
 
-    return {total};
+    // Mostrar tirada de menor cantidad de defectos
+    for (int tirada = 1; tirada < tiradas; tirada++){
+        if(total_por_tiradas[tirada-1] < total_por_tiradas[tirada]) {
+            tirada_menor = tirada-1;
+        }
+        else tirada_menor = tirada;
+        
+        if(total_por_tiradas[tirada-1] > total_por_tiradas[tirada]) {
+            tirada_mayor = tirada-1;
+        }
+        else tirada_mayor = tirada;
+    }
+
+    return {total, tirada_mayor+1, tirada_menor+1};
 
 }
 
@@ -51,7 +71,9 @@ int main(){
  
     Resultados resultados = Funcionalidades(tiradas, cant_productos, defectos);
     //Imprimiendo Total
-    cout<<"El total de productos es: "<<resultados.total;
+    cout<<"El total de productos es: "<<resultados.total<<endl;
+    cout<<"La tirada con menor defectos en sus productos fue: "<<resultados.menor<<endl;
+    cout<<"La tirada con mayor defectos en sus productos fue: "<<resultados.mayor<<endl;
 
     return 0;
 }
