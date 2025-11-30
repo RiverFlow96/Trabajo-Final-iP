1efe3202501fbcfc5bace897ebfb076e0620ffe8
RiverFlow <yordanyalmeda8@gmail.com>
2025-11-30 08:26:49 -0500

    Modificaciones agregadas: se corrigieron errores en la funcion Funcionalidades, se cambio el array bidimensional por un vector de vectores, se agregan los valores por consola en lugar de inicializarlos, al igual que las variables tiradas y cant_productos

--- main.cpp
+++ main.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Resultados
+{
+    int total;
+};
+
+
 //Funcion para calcular el total
-int total(int array){
+Resultados Funcionalidades(int tiradas, int cant_productos, vector<vector<int>>& array){
     int total = 0;
 
-    for (int tirada = 0; tirada < 3; tirada++){
-        for(int producto = 0; producto < 4; producto++){
-            total += producto;
+    for (int tirada = 0; tirada < tiradas; tirada++){
+        for(int producto = 0; producto < cant_productos; producto++){
+            total += array[tirada][producto];
         }
     }
 
-    return total;
+    return {total};
 
 }
 
 int main(){
 
-    const int tiradas = 3;
-    const int cant_productos = 4;
+    int tiradas = 3;
+    int cant_productos = 4;
 
-    //Array de defectos
-    int defectos[tiradas][cant_productos] = {
-        {1, 2, 3, 4}, 
-        {1, 2, 3, 4}, 
-        {1, 2, 3, 4}
-    };
+    // Valores por pantalla
+    cout<<"Ingrese la cantidad de tiradas: ";
+    cin>>tiradas;
 
+    cout<<"Ingrese la cantidad de productos por tiradas: ";
+    cin>>cant_productos;
+    
+    // Inicializando vector de defectos
+    vector<vector<int>> defectos(tiradas, vector<int>(cant_productos)); 
+
+    cout<<"CANTIDAD DE DEFECTOS"<<endl;
+
+    // Recorriendo el vector y rellenando
+    for(int i = 0; i < tiradas; i++){
+        cout<<"---Tirada "<<i+1<<"---"<<endl;
+        for(int j = 0; j < cant_productos; j++){
+            cout<<"Producto "<<j+1<<": ";
+            cin>>defectos[i][j];
+        }
+    }
+ 
+    Resultados resultados = Funcionalidades(tiradas, cant_productos, defectos);
     //Imprimiendo Total
-    cout<<total(defectos[tiradas][cant_productos]);
+    cout<<"El total de productos es: "<<resultados.total;
 
     return 0;
 }
